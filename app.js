cb4794887577cffcf345876ab899958f4fdcafb5
Fredrica <fredricaarthurmaame@gmail.com>
2021-08-21 01:16:50 +0000

    Incorporated the template engine along with other
    third party packages.
    I created a views folder to keeo all my routes or pages with the
    extension .ejs.
    I also created the public folder to hold the styles and the images that
    will be used in the application.

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 const express = require('express');
+const morgan = require('morgan');
+
 
 //creating th express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs')
+
 //listening for requests
 app.listen(3000);
 
+//middleware and static files
+app.use(express.static('public'));
+
+
 app.get('/',(req,res) => {
-    res.sendFile('./index.html',{root: __dirname});
+    res.render('index');
 });
 
-app.get('/log-in',(req,res) => {
-    res.sendFile('./pages/log-in.html',{root: __dirname});
+app.get('/log-in.ejs',(req,res) => {
+    res.render('log-in');
 });
 
-app.get('/sign-up',(req,res) => {
-    res.sendFile('./pages/sign-up.html',{root: __dirname});
+app.get('/sign-up.ejs',(req,res) => {
+    res.render('sign-up');
 });
 
-app.get('/user-dashboard',(req,res) => {
-    res.sendFile('./pages/user-dashboard.html',{root: __dirname});
+app.get('/user-dashboard.ejs',(req,res) => {
+    res.render('user-dashboard');
 });
 
-app.get('/add-task',(req,res) => {
-    res.sendFile('./pages/add-task.html',{root: __dirname});
+app.get('/add-task.ejs',(req,res) => {
+    res.render('add-task');
 });
 
 //404 page
 app.use((req,res) => {
-    res.status(404).sendFile('./pages/404.html', {root: __dirname})
+    res.status(404).render('404')
 });
 
 
