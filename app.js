a71511b95bcc8ceedff2b85de93bf21d03e712f1
Fredrica <fredricaarthurmaame@gmail.com>
2021-08-21 19:08:00 +0000

    Connected to cloud mongodb database

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 
 //creating th express app
 const app = express();
 
+//connect to mongodb
+const dbURI = 'mongodb+srv://freddy:testone@checklist.mckxo.mongodb.net/Checklist?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine', 'ejs')
 
 //listening for requests
-app.listen(3000);
+
 
 //middleware and static files
 app.use(express.static('public'));
