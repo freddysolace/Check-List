919598872a7022b76263009c8cbbe5d64380207a
Fredrica <fredricaarthurmaame@gmail.com>
2021-08-29 13:36:53 +0000

    Added login feature

--- api/handlers/user.js
+++ api/handlers/user.js
@@ -1,42 +1,64 @@
 const User = require('../models/user');
 const app = require('express')()
+const path = require('path');
 
 app.set('views', '../../views');
 app.set('view engine', 'ejs');
 
 exports.sign_up = (req, res, next) => {
 
-    User.find({email: req.body.email}).exec()
-        .then(user => {
-            console.log(user)
-            if(user.length >=1) {
-                const error = {
-                    path: 'sign-up',
-                    message: 'Email already in use'
-                }
-                res.error = error
-                res.redirect('/error');
-                return;
-            }
-        })
+//     User.find({email: req.body.email}).exec()
+//         .then(user => {
+//             console.log(user)
+//             if(user.length >=1) {
+//                 const error = {
+//                     // path: 'sign-up',
+//                     message: 'Email already in use'
+//                 }
+//                 // res.error = error
+//                 // res.render(path.join(__dirname,'views','sign-up'),[error]);
+//                 return;
+//             }
+//         })
     
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
     
 
-    // const newuser = new user();
-    // newuser.username = username;
-    // newuser.email = email;
-    // newuser.password = password;
-    // newuser.save((err,savedUser) => {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         console.log(savedUser);
-    //         res.redirect('/login');
-    //     }
-    // });
+    const newuser = new User({
+        username : username,
+        email : email,
+        password : password
+    });
+    newuser.save((err,savedUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(savedUser);
+            res.redirect('/login');
+        }
+    });
+}
+
+exports.log_in = (req,res,next) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    const user = new User({
+        email : email,
+        password : password
+    });
+
+    User.find({email: email,password: password}).exec()
+        .then(user => {
+            if (!user) {
+                res.redirect("/404");
+            }
+            res.redirect("/user-dashboard");
+        })
 }
 
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userHandler = require('../handlers/user')
 
 router.post('/sign-up', userHandler.sign_up);
-router.post('/login');
+router.post('/login',userHandler.log_in);
 router.post('/delete-account');
 
 module.exports = router;
